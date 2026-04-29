6c64252ac30cb991d27eb61d03fc9387c896b298
selcukdinc <selcukdinc2508@hotmail.com>
2026-04-29 16:01:21 +0300

    Initial Commit

---
+++ MirrorTop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MirrorTop
+//
+//  Created by Selçuk DİNÇ on 29.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MirrorTop/MirrorTopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MirrorTopApp.swift
+//  MirrorTop
+//
+//  Created by Selçuk DİNÇ on 29.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct MirrorTopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
